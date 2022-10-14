28e5e41d5e77ca7f00ed735d309290dc22740942
jumroon <jumroon.rada@gmail.com>
2022-10-14 20:50:45 +0200

    breadcrumbs made, new card page exists

---
+++ src/Layout/Breadcrumbs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Breadcrumbs() {
+  return (
+    <div>
+      <span>Home</span>
+      <span>Card</span>
+      <span>Add Card</span>
+    </div>
+  );
+}
--- src/Layout/NewCardPage.js
+++ src/Layout/NewCardPage.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { readDeck } from "../utils/api";
+import { Breadcrumbs } from "./Breadcrumbs";
+import { LoadingIndicator } from "./LoadingIndicator";
 
 export function NewCardPage() {
-  return <h1>I am a happy card page lol</h1>;
+  const [deck, setDeck] = useState();
+  const { deckId } = useParams();
+
+  useEffect(() => {
+    async function getDeck() {
+      const newDeck = await readDeck(deckId);
+      setDeck(newDeck);
+      console.log(newDeck);
+    }
+    getDeck();
+  }, []);
+
+  if (!deck) {
+    return <LoadingIndicator />;
+  }
+
+  return (
+    <>
+      <Breadcrumbs />
+      <h1>I am a happy card page lol</h1>
+      <h3>{deck.name}</h3>
+
+      <h3>front</h3>
+      <textarea></textarea>
+      <h3>back</h3>
+      <textarea></textarea>
+      <button>Save</button>
+    </>
+  );
 }
