154b48e2bccc79bb44edcb104d7c2801346b22cd
jumroon <jumroon.rada@gmail.com>
2022-10-15 14:23:44 +0200

    we got the card we want

--- src/Layout/CardEditPage.js
+++ src/Layout/CardEditPage.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useReadDeck } from "../utils/hooks";
+import { LoadingIndicator } from "./LoadingIndicator";
+import { useParams } from "react-router-dom";
 
 export function CardEditPage() {
+  const { cardId } = useParams();
+
+  const deck = useReadDeck();
+
+  if (!deck) {
+    return <LoadingIndicator />;
+  }
+
+  console.log(cardId);
+  const cardWeWant = deck.cards.find((card) => card.id == cardId);
+  console.log(cardWeWant);
+
   return <div> super happy fun card editing </div>;
 }
