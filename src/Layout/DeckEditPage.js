de4c3209edc5a3439868d4e97655eaa6dfbf6933
jumroon <jumroon.rada@gmail.com>
2022-10-17 20:47:39 +0200

    cancel button on deckEdit reroutes to decks/deckId:

--- src/Layout/DeckEditPage.js
+++ src/Layout/DeckEditPage.js
@@ -2,13 +2,20 @@ import React from "react";
 import { useReadDeck } from "../utils/hooks";
 import { DeckForm } from "./DeckForm";
 import { LoadingIndicator } from "./LoadingIndicator";
+import { useHistory } from "react-router-dom";
 
 export function DeckEditPage() {
+  const history = useHistory();
   const deck = useReadDeck();
 
   if (!deck) {
     return <LoadingIndicator />;
   }
 
-  return <DeckForm deck={deck} />;
+  console.log(deck.id);
+  function onCancel() {
+    history.push(`/decks/${deck.id}`);
+  }
+
+  return <DeckForm deck={deck} onCancel={onCancel} />;
 }
