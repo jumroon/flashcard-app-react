1741dd6dc850babf7f20c255e4e997ee62080806
jumroon <jumroon.rada@gmail.com>
2022-10-19 16:26:46 +0200

    delete button on homepage is functional

--- src/Layout/HomePage.js
+++ src/Layout/HomePage.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useHistory } from "react-router-dom";
+import { useReadDeck } from "../utils/hooks";
 import Deck from "./Deck";
-import { listDecks } from "../utils/api";
+import { listDecks, deleteDeck, updateDeck } from "../utils/api";
 import { StudyPage } from "./StudyPage";
+import { LoadingIndicator } from "./LoadingIndicator";
 
 export function HomePage() {
   const [decks, setDecks] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     async function callListOfDecks() {
@@ -15,6 +17,19 @@ export function HomePage() {
     }
     callListOfDecks();
   }, []);
+
+  const deleteHandler = async (deckId) => {
+    if (
+      window.confirm(
+        "Are you sure you want to delete this deck? You will not be able to recover it."
+      )
+    ) {
+      await deleteDeck(deckId);
+      history.go(0);
+    } else {
+      history.go(0);
+    }
+  };
 
   const deckNames = decks.map((deck, index) => (
     <>
@@ -26,7 +41,7 @@ export function HomePage() {
       <Link to={`/decks/${index + 1}/edit`}>
         <button>Edit</button>
       </Link>
-      <button>Delete</button>
+      <button onClick={() => deleteHandler(deck.id)}>Delete1111</button>
     </>
   ));
 
