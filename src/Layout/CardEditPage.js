3b11bb669e9f4de05a86aa19593da7eab1d8b0b2
jumroon <jumroon.rada@gmail.com>
2022-10-20 11:20:22 +0200

    editDeckPage is fully functional

--- src/Layout/CardEditPage.js
+++ src/Layout/CardEditPage.js
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useReadDeck } from "../utils/hooks";
 import { LoadingIndicator } from "./LoadingIndicator";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useHistory } from "react-router-dom";
+import { updateCard } from "../utils/api";
 
 export function CardEditPage() {
   const { cardId } = useParams();
-
   const [deck] = useReadDeck();
+  const [card, setCard] = useState();
+  const history = useHistory();
 
-  if (!deck) {
+  useEffect(() => {
+    if (deck) {
+      const cardWeWant = deck.cards.find((card) => card.id == cardId);
+      setCard(cardWeWant);
+    }
+  }, [deck]);
+
+  if (!deck || !card) {
     return <LoadingIndicator />;
   }
 
-  const cardWeWant = deck.cards.find((card) => card.id == cardId);
+  async function onSubmit(event) {
+    event.preventDefault();
+    await updateCard(card);
+    history.push(`/decks/${deck.id}`);
+  }
+
+  const handleChange = (event) => {
+    const { target } = event;
+    setCard({ ...card, [target.id]: target.value });
+  };
 
   return (
     <>
-      <h1>Edit Card</h1>
-      <h3>Front</h3>
-      <textarea value={cardWeWant.front}></textarea>
-      <h3>Back</h3>
-      <textarea value={cardWeWant.back}></textarea>
-      <Link to={`/decks/${deck.id}`}>
-        <button>Cancel</button>
+      <form onSubmit={(event) => onSubmit(event)}>
+        <h1>Edit Card</h1>
+        <h3>Front</h3>
+        <textarea
+          id="front"
+          value={card.front}
+          onChange={handleChange}></textarea>
+        <h3>Back</h3>
+        <textarea
+          id="back"
+          value={card.back}
+          onChange={handleChange}></textarea>
+        <Link to={`/decks/${deck.id}`}>
+          <button>Cancel</button>
+        </Link>
         <button>Submit</button>
-      </Link>
+      </form>
     </>
   );
 }
