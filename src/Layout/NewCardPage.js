62864894f94e9697d2620068199b00511f82483c
jumroon <jumroon.rada@gmail.com>
2022-10-20 10:35:01 +0200

    new card page: done button goes back to deckpage, save button adds card and refreshes the page

--- src/Layout/NewCardPage.js
+++ src/Layout/NewCardPage.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Breadcrumbs } from "./Breadcrumbs";
 import { LoadingIndicator } from "./LoadingIndicator";
 import { useReadDeck } from "../utils/hooks";
-import CardForm from "./CardForm__TEMPLATE";
-
+import { createCard } from "../utils/api";
+import { useHistory, Link } from "react-router-dom";
 export function NewCardPage() {
   const [deck] = useReadDeck();
+  const [card, setCard] = useState({ front: "", back: "" });
+  const history = useHistory();
 
   if (!deck) {
     return <LoadingIndicator />;
   }
+  const cards = deck.cards;
+  async function addCard() {
+    await createCard(deck.id, card);
+    history.go(0);
+  }
 
+  const handleChange = (event) => {
+    const { target } = event;
+    setCard({ ...cards, [target.id]: target.value });
+  };
+
+  console.log("DECK AND CARDS LABEL", deck, deck.cards);
   return (
     <>
       <Breadcrumbs />
 
       <h1>I am a happy card page lol</h1>
-      <h3>{deck.name}</h3>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addCard();
+        }}>
+        <h2>{deck.name}</h2>
 
-      <h3>front</h3>
-      <textarea></textarea>
-      <h3>back</h3>
-      <textarea></textarea>
-      <button>Save</button>
+        <h3>front</h3>
+        <textarea
+          id="front"
+          value={card.front}
+          onChange={handleChange}></textarea>
+        <h3>back</h3>
+        <textarea
+          id="back"
+          value={card.back}
+          onChange={handleChange}></textarea>
+        <button>Save</button>
+        <Link to={`/decks/${deck.id}`}>
+          <button>Done</button>
+        </Link>
+      </form>
     </>
   );
 }
